0e0a1b83c35f75b6dcd29a1bc8a51a256acb29d7
Romaincapp <romainfrancedumoulin@gmail.com>
2025-02-20 16:33:43 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CampingReservation from './path/to/CampingReservation';
+
+ReactDOM.render(<CampingReservation />, document.getElementById('root'));
